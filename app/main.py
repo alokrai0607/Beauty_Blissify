87f20bb4ab189374c284b675e0d703da5ecec507
alokrai0607 <saelalok001@gmail.com>
2024-11-09 13:49:29 +0530

    Initial commit

--- app/main.py
+++ app/main.py
@@ -28,6 +28,8 @@ models.Base.metadata.create_all(bind=database.engine)
 app.include_router(product_router, prefix="/api/v1")
 
 # .\venv\Scripts\Activate
+
 # .\venv\Scripts\uvicorn.exe app.main:app --reload
+
 #  pytest
 

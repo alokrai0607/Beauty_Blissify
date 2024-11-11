6b298907dc7f1f8e1f6242f07e1df810c894bdbf
alokrai0607 <saelalok001@gmail.com>
2024-11-11 11:25:10 +0530

    Beauty_Blissify

--- app/main.py
+++ app/main.py
@@ -4,16 +4,9 @@ from .routers import router as product_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from dotenv import load_dotenv
-import os
-import uvicorn
 
 app = FastAPI()
 
-# Mount the Frontend folder to serve static files
-app.mount("/static", StaticFiles(directory="Frontend"), name="static")
-
-# Serve the main HTML file at the root
 @app.get("/")
 def read_root():
     return FileResponse("Frontend/index.html")
@@ -28,12 +21,4 @@ app.add_middleware(
 
 models.Base.metadata.create_all(bind=database.engine)
 app.include_router(product_router, prefix="/api/v1")
-
-load_dotenv()
-
-Port = int(os.get('PORT',8000))
-Host = '0.0.0.0'
-
-if __name__ == '__main__':
-    uvicorn.run('app.main:app' , host = Host ,port = Port ,reload = True)
 
